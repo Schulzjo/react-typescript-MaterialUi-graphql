786d2297a8ab3c3462e0128cdbe28417784eb5a6
schulzjo <schulzeeverding.johannes@gmail.com>
2021-08-24 15:41:25 +0200

    remove old components

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
-import './index.css';
 import AppMui from './AppMui';
 
 const client = new ApolloClient({
